2181b16bb7670fc54f112ccf33dc7ff2b62c47ff
Nina Potrebich <potrebich@gmail.com>
2019-11-13 23:20:26 -0800

    correct project

--- src/main.js
+++ src/main.js
@@ -1,13 +1,37 @@
 import $ from 'jquery';
 import 'bootstrap';
 import './styles.css';
-import { Card, Deck } from './memory';
+import { Deck } from './memory';
 
 
 $(document).ready(function() {
   const deck = new Deck();
   deck.shuffle();
   deck.cards.forEach((element,idx) => {
-    $("#" + idx).html("<img src='../src/img/" + element.getImage() + "' alt='image " + element.value + "'>");
+    if (element.state) {
+      $("#" + (idx+1)).html("<img src='./../src/img/" + element.getImage() + "' alt='image " + element.value + "'>");
+    }
+  });
+
+  $("#game").click(function(event) {
+    if (deck.play(event.target.id-1)) {
+      let previous = deck.previous;
+      let idx = event.target.id;
+      $("#" + (idx)).html("<img src='./../src/img/" + deck.cards[idx-1].getImage() + "' alt='image " + deck.cards[idx-1].value + "'>");
+      $("#" + (previous+1)).html("<img src='./../src/img/" + deck.cards[previous].getImage() + "' alt='image " + deck.cards[previous].value + "'>");
+      setTimeout(function() {
+        $("#" + (idx)).html("");
+        $("#" + (previous+1)).html("");
+      }, 1000)
+    } else {
+      deck.cards.forEach((element,idx) => {
+        if (element.state) {
+          $("#" + (idx+1)).html("<img src='./../src/img/" + element.getImage() + "' alt='image " + element.value + "'>");
+        } else {
+          $("#" + (idx+1)).html("");
+        }
+      });
+    }
+    
   });
 });
--- src/memory.js
+++ src/memory.js
@@ -1,20 +1,25 @@
+import $ from 'jquery';
+
 export function Card(value) {
   this.value = value;
+  this.state = false;
 }
 
 Card.prototype.getImage = function() {
   return this.value + ".jpg";
-} 
+}; 
 
 
 export function Deck() {
-  this.cards = [new Card(1), new Card(1), new Card(2), new Card(2), new Card(3), new Card(3), new Card(4), new Card(4), new Card(5), new Card(5)];
+  this.cards = [new Card(1), new Card(1), new Card(2), new Card(2), new Card(3), new Card(3), new Card(4), new Card(4), new Card(5), new Card(5)],
+  this.state = false,
+  this.previous = false;
 }
 
 Deck.prototype.shuffle = function () {
   let shuffledCards = [];
   let count = this.cards.length-1;
-  while (count > 0) {
+  while (count >= 0) {
     var randomIndex = Math.round(Math.random()*(this.cards.length-1));
     if (this.cards[randomIndex]) {
       shuffledCards.push(this.cards[randomIndex]);
@@ -23,4 +28,31 @@ Deck.prototype.shuffle = function () {
     }
   }
   this.cards = shuffledCards;
+};
+
+Deck.prototype.play = function(index) {
+  if (index == -1) {
+    return false
+  }
+  if (!this.cards[index].state) {
+    if (this.state) {
+      if (this.cards[this.previous].value === this.cards[index].value) {
+        this.cards[index].state = true;
+        this.previous = false;
+        this.state = false;
+      } else {
+        const temp = this.previous;
+        this.cards[index].state = false;
+        this.cards[this.previous].state = false;
+        this.state = false;
+        return temp
+      }
+    
+    } else {
+      this.previous = index;
+      this.state = true;
+      this.cards[this.previous].state = true; 
+    }
+    return false;
+  }
 };
